48bac7503a625835c8c1c9fdc6f05054fce60e3a
Jonathan MirCha <Jonathan MirCha>
2016-04-21 23:32:07 -0500

    Clase 2

--- JS/js/00_clases_objetos.js
+++ JS/js/00_clases_objetos.js
@@ -5,6 +5,7 @@ multilinea
 */
 //función anónima autoejecutable
 (function (d, w, n) {
+	'use strict';
 	//console.log(d, w, n);
 
 	//Objeto literal
@@ -67,17 +68,93 @@ multilinea
 	perro2.morder('juguete');
 	perro.aparecer('https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/German_Shepherd_Dog_head_Lamia_2005.jpg/220px-German_Shepherd_Dog_head_Lamia_2005.jpg');
 
+	//Función Constructora
+	function Perro (nombre, edad, raza, genero, esterilizado) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.raza = raza;
+		//operador ternario
+		//variable = (condición) ? verdadero : falso;
+		// = Asignación
+		// == Comparo valores
+		//this.genero = (genero == true) ? 'Hembra' : 'Macho';
+		// === Comparo valor y tipo
+		//this.genero = (genero === true) ? 'Hembra' : 'Macho';
+		this.genero = (genero) ? 'Hembra' : 'Macho';
+		this.esterilizado = esterilizado;
 
+		this.ladrar = function () {
+			alert('guau guau');
+		};
 
-
+		this.morder = function (cosa) {
+			alert('Voy a morder ' + cosa);
+		};
 	
+		this.aparecer = function (imagen) {
+			d.write('<img src="' + imagen +'">');
+		};
+	}
+
+	var perro3 = new Perro('Toti', 15, 'French', 0, false);
+	var perro3_2 = new Perro('Hérculisa', 4, 'Chihuahua', 1, true);
+	console.log(
+		perro3.nombre,
+		perro3.edad,
+		perro3.genero,
+		perro3.esterilizado,
+		perro3.raza,
+		perro3_2.nombre,
+		perro3_2.edad,
+		perro3_2.genero,
+		perro3_2.esterilizado,
+		perro3_2.raza
+	);
+
+	perro3.ladrar();
+	perro3.morder('cartero');
+	perro3.aparecer('http://www.perrosamigos.com/Uploads/perrosamigos.com/ImagenesGrandes/caniche-2.jpg');
+
+	perro3_2.ladrar();
+	perro3_2.morder('piedra');
+	perro3_2.aparecer('http://previews.123rf.com/images/isselee/isselee1205/isselee120500551/13589411-Cachorro-de-Chihuahua-11-semanas-de-edad-sentado-delante-de-fondo-blanco-Foto-de-archivo.jpg');
+
+	//Clases (apartir de ES6 o ES2015)
+	class PerroClass {
+		//El constructor es un método especial que se ejecuta en el momento de instanciar la clase
+		constructor(nombre, edad, raza, genero, esterilizado) {
+			this.nombre = nombre;
+			this.edad = edad;
+			this.raza = raza;
+			this.genero = (genero) ? 'Hembra' : 'Macho';
+			this.esterilizado = esterilizado;
+		}
+
+		ladrar() {
+			alert('guau guau');
+		};
+
+		morder(cosa) {
+			alert('Voy a morder ' + cosa);
+		};
+	
+		aparecer(imagen) {
+			d.write('<img src="' + imagen +'">');
+		};
+	}
+
+	var perro4 = new PerroClass('Firulais', 6, 'Border Collie', false, true);
+
+	console.log(
+		perro4.nombre,
+		perro4.edad,
+		perro4.genero,
+		perro4.esterilizado,
+		perro4.raza
+	);
+
+	perro4.ladrar();
+	perro4.morder('cartero');
+	perro4.aparecer('http://1.bp.blogspot.com/-ac2NHP5U5i0/T10WkHQcsSI/AAAAAAAAA4k/5MnFLni7Y0s/s1600/border+collie+cachorro.jpg');
 
 })(document, window, navigator);
-		
-
-
-
-
-
-		//operador ternario
-		//variable = (condición) ? true : false;

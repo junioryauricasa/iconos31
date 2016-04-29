00e80012f435a9637fc4d3e884a84aa7381dd6f7
Jonathan MirCha <Jonathan MirCha>
2016-04-28 22:02:21 -0500

    Clase 3

--- JS/js/02_eventos.js
+++ JS/js/02_eventos.js
@@ -54,3 +54,44 @@ function rojo(evento) {
 		btnAzul.removeEventListener('click', azul);
 	});
 })(document);
+
+(function (d, w) {
+	var fecha = new Date(),
+		hora = fecha.getHours(),
+		saludo = d.getElementById('saludo'),
+		hojaCSS = d.createElement('link');
+
+	function saludar() {
+		/*
+			No me jodas 0-5
+			Buenos dias 6-11
+			Buenas tardes 12-18 
+			Buenas noches 19-23
+		*/
+		//if (hora < 6)
+		if (hora <= 5) {
+			saludo.innerHTML = '<h3>No me jodas!!!</h3>';
+			hojaCSS.href = './css/duermete.css';
+		} else if(hora >= 6 && hora <= 11) {
+			saludo.innerHTML = '<h3>Buenos días!!</h3>';
+			hojaCSS.href = './css/dia.css';
+		} else if(hora >= 12 && hora <= 18) {
+			saludo.innerHTML = '<h3>Buenas tardes!!</h3>';
+			hojaCSS.href = './css/tarde.css';
+		} else {
+			saludo.innerHTML = '<h3>Buenas noches!!</h3>';
+			hojaCSS.href = './css/noche.css';
+		}
+
+		saludo.style.fontSize = '300%';
+		hojaCSS.rel = 'stylesheet';
+		d.head.appendChild(hojaCSS);
+	}
+
+	console.log(
+		fecha,
+		hora
+	);
+
+	w.onload = saludar;
+})(document, window);

36577eac5439e4caf1c06391f82a986c204594d7
Jonathan MirCha <Jonathan MirCha>
2016-04-28 20:59:16 -0500

    Clase 3

--- JS/js/01_dom.js
+++ JS/js/01_dom.js
@@ -32,8 +32,18 @@
 	document.querySelectorAll('li');
 	document.querySelector('li');
 
-	var lista = d.getElementById('lista');
+	var lista = d.getElementById('lista'),
+		li = d.createElement('li'),
+		txtLi = d.createTextNode('Lista 6'),
+		p1 = d.querySelector('#uno'),
+		p2 = d.querySelector('#dos'),
+		p3,
+		section = d.querySelector('#container'),
+		otroLi,
+		a = d.querySelector('.link'),
+		html = d.querySelector('html');
 
+	/*
 	console.log(
 		lista,
 		lista.parentNode,
@@ -49,4 +59,56 @@
 		lista.nextElementSibling,
 		lista.nextSibling
 	);
+	*/
+
+	li.appendChild(txtLi);
+	lista.appendChild(li);
+
+	p3 = p2.cloneNode(true);
+	section.appendChild(p3);
+
+	lista.removeChild(lista.firstElementChild);
+
+	otroLi = li.cloneNode(false);
+	lista.insertBefore(otroLi, lista.children[0]);
+	otroLi.innerText = '<b>Lista 7</b>';
+
+	p1.innerHTML = '<b>Hola, este es el contenido del párrafo 1</b>';
+
+	lista.firstChild.nodeValue = 'Hola soy el primer nodo';
+
+	lista.replaceChild(lista.children[0], lista.children[3]);
+
+	console.log(
+		section.id,
+		section.className,
+		section.classList,
+		section.classList[1],
+		section.classList.contains('foo'),
+		section.classList.contains('fixed'),
+		a.href,
+		a.getAttribute('target'),
+		a.style,
+		d.head,
+		d.body
+	);
+
+	a.href = 'http://jonmircha.com';
+	a.setAttribute('target', '_self');
+
+	html.style.fontSize = '32px';
+	a.style.padding = '1rem';
+	a.style.borderRadius = '.25rem';
+
+	p3.setAttribute('id','tres');
+
+	d.querySelector('title').innerText = 'Hola';
+	d.body.style.backgroundColor = 'black';
+	d.body.style.color = 'greenyellow';
+	
+	container.style.backgroundColor = 'skyblue';
+	container.insertAdjacentHTML('beforebegin', '<div>Contenido dinámico agregado antes y afuera del id container</div>');
+	container.insertAdjacentHTML('afterbegin', '<div>Contenido dinámico agregado antes y dentro del id container</div>');
+	container.insertAdjacentHTML('beforeend', '<div>Contenido dinámico agregado después y dentro del id container</div>');
+	container.insertAdjacentHTML('afterend', '<div>Contenido dinámico agregado después y afuera del id container</div>');
 })(document);
---
+++ JS/js/02_eventos.js
@@ -0,0 +1,8 @@
+(function (d) {
+	'use strict';
+
+	var btnVerde = d.querySelector('#verde'),
+		btnAzul = d.getElementById('azul'),
+		btnReset = d.querySelector('#resetear'),
+		btns = d.querySelectorAll('button');
+})(document);

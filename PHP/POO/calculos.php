9517560dce8331569af6bb92f8a61fde1e4dd061
Jonathan MirCha <Jonathan MirCha>
2016-06-30 20:57:04 -0500

    Clase 1 PHP

---
+++ PHP/POO/calculos.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+Tipos de clases en PHP:
+		Instanciables
+			Ser instanciada (se pueden crear objetos) y/o se pueden heredar
+		Heredadas
+			Guardar relación con métodos y atributos de otra
+		Finales
+			Es Instanciables pero no se puede heredar
+		Abstractas
+			Sirven de modelo para otras, NO se pueden instanciar y DEBEN heredarse
+*/

2808bc2393f6b69c318dc158d4b2fd25e8a6b956
Jonathan MirCha <jonmircha@gmail.com>
2016-07-14 20:14:28 -0500

    Clase 3 PHP

--- PHP/Mexflix/index.php
+++ PHP/Mexflix/index.php
@@ -1,7 +1,31 @@
-<?php 
-require_once('./models/Model.php');
-require_once('./models/StatusModel.php');
-
-$status = new StatusModel();
-
-var_dump( $status->get() );
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<title>Mexflix</title>
+	<link rel="shortcut icon" type="image/png" href="./public/img/favicon.png">
+	<link rel="stylesheet" href="./public/css/responsimple.min.css">
+	<link rel="stylesheet" href="./public/css/mexflix.css">
+</head>
+<body>
+	<header class="container center header">
+		<div class="item  i-b  v-middle  ph12  lg2">
+			<h1 class="logo">Mexflix</h1>
+		</div>
+		<nav class="item  i-b  v-middle  ph12  lg10  lg-right">
+			<ul class="container">
+				<li class="nobullet  item  block  lg-inline"><a href="./">Inicio</a></li>
+				<li class="nobullet  item  block  lg-inline"><a href="movieseries">MovieSeries</a></li>
+				<li class="nobullet  item  block  lg-inline"><a href="usuarios">Usuarios</a></li>
+				<li class="nobullet  item  block  lg-inline"><a href="status">Status</a></li>
+				<li class="nobullet  item  block  lg-inline"><a href="salir">Salir</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main class="container center main">
+		<p>Aquí trabajaremos</p>
+	</main>
+	<script src="./public/js/mexflix.js"></script>
+</body>
+</html>

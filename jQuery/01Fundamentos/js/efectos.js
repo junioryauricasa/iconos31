b939e89e2ae8bf05d58e2d9773b11789a62052f7
Jonathan MirCha <jonmircha@gmail.com>
2016-08-18 20:16:17 -0500

    Clase 2 jQuery

--- jQuery/01Fundamentos/js/efectos.js
+++ jQuery/01Fundamentos/js/efectos.js
@@ -159,7 +159,128 @@
 			$('#contenido').after('<div class="despues">Contenido agregado después del selector</div>');
 			$('.despues').css('background-color', '#205081');
 		});
+
+		$('#enlace').on({
+			click : function (evento) {
+				evento.preventDefault();
+				alert('Se ha prevenido la acción por defecto del enlace');
+			},
+			mouseover : function () {
+				//$('span').addClass('mas-grande');
+				$('span').toggleClass('mas-grande');
+			},
+			mouseout : function () {
+				//$('span').removeClass('mas-grande');
+				$('span').toggleClass('mas-grande');
+			}
+		});
+
+		$('#boton18').click(function () {
+			/*
+			$('#oculto').hide(2000);
+			alert('El párrafo se ha ocultado');
+			*/
+
+			//función callback
+			$('#oculto').hide(2000, function () {
+				alert('El párrafo se ha ocultado');	
+			});
+		});
+		
+		$('#boton19').click(function () {
+			//$('#ajax').load('./otro.html');
+			$('#ajax').load('./otro.html #videos');
+		});
+		
+		$('#boton20').click(function () {
+			$('#ajax').load('./otro.html', function () {
+				$(this)
+					.css({display:'none'})
+					.fadeIn(2000);
+			});
+		});
+
+		$('#ajax').css({width:2000});
+
+		$('#nombre').focus(function () {
+			$('#saludo').text('¿Cómo te llamas?');
+		});
+
+		$('#nombre').blur(function () {
+			$('#saludo').text( 'Hola ' + $('#nombre').val() );
+			$('#nombre').val(null);
+		});
+
+		$('#subir').click(function () {
+			$('body').animate({
+				scrollTop : 0,
+				scrollLeft : 0
+			}, 1000);
+		});
+	}//cierra efectos
+
+	function muevete(e) {
+		//console.log(e);
+		//console.log(e.keyCode);
+
+		switch (e.keyCode) {
+			case 37 : {
+				e.preventDefault();
+				$('#pacman').animate({left:'-=2rem'}, 'swing');
+				break;
+			}
+
+			case 38 : {
+				e.preventDefault();
+				$('#pacman').animate({top : '-=2rem'}, 'swing');
+				break;
+			}
+
+			case 39 : {
+				e.preventDefault();
+				$('#pacman').animate({left:'+=2rem'}, 'swing');
+				break;
+			}
+
+			case 40 : {
+				e.preventDefault();
+				$('#pacman').animate({top : '+=2rem'}, 'swing');
+				break;
+			}
+		}//cierra switch
+	}//cierra muevete
+
+	function detectarScroll () {
+		var scrollVertical = $(window).scrollTop(),
+			scrollHorizontal = $(window).scrollLeft();
+
+		//console.log( scrollVertical, scrollHorizontal );
+
+		return (scrollVertical > 100)
+			? $('#subir').fadeIn()
+			: $('#subir').fadeOut();
+	}
+
+	function webPerformanceOptimization () {
+		var anchoPantalla = $(window).innerWidth(),
+			altoPantalla = $(window).innerHeight(),
+			contenidoDesktop = '<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLvq-jIkSeTUYvLDfVUXOhnZ6QSouIfQQ7" frameborder="0" allowfullscreen></iframe>',
+			contenidoMobile = '<a href="https://www.youtube.com/playlist?list=PLvq-jIkSeTUYvLDfVUXOhnZ6QSouIfQQ7" target="_blank">Ver en youtube</a>';
+
+		//console.log( anchoPantalla, altoPantalla );
+
+		return (anchoPantalla <= 1024)
+			? $('#wpo').html(contenidoMobile)
+			: $('#wpo').html(contenidoDesktop);
 	}
 
 	$(document).ready(efectos);
+	$(document).keydown(muevete);
+
+	//$(window).scroll(detectarScroll);
+	$(window).on({
+		scroll : detectarScroll,
+		resize : webPerformanceOptimization,
+		load : webPerformanceOptimization
+	});
 })(jQuery);

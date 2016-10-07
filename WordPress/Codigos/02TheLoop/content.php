c568134a8b97c1a75efd1592de5f568a1bf74512
Jonathan MirCha <jonmircha@gmail.com>
2016-10-06 21:22:05 -0500

    Clase 3 WordPress

---
+++ WordPress/Codigos/02TheLoop/content.php
@@ -0,0 +1,40 @@
+<?php 
+print('<main class="main  flex-auto  sm-w100  lg-w70  b-dark-gray  border">');
+	if( have_posts() ):
+		while( have_posts() ):
+			the_post();
+			//post info
+			//print('<p>Si hay publicaciones</p>');
+			//the_title();
+			$html = '
+				<article class="post">
+					<h2>%s</h2>
+					<a href="%s">Ver publicación</a>
+					<p>%s -- %s</p>
+					<p>%s</p>
+					<p class="post-categories">%s</p>
+					<p>%s</p>
+					<p>%s</p>
+					<div class="the-content">%s</div>
+				</article>
+				<hr>
+			';
+
+			printf(
+				$html,
+				get_the_title(),
+				get_the_permalink(),
+				get_the_date(),
+				get_the_time(),
+				get_the_excerpt(),
+				get_the_category_list(' - '),
+				get_the_tag_list('<i>', ', ', '</i>'),
+				get_the_author(),
+				get_the_content()
+			);
+		endwhile;
+	else:
+		//no posts
+		print('<p class="error">No hay publicaciones</p>');
+	endif;	
+print('</main>');
---
+++ WordPress/Codigos/02TheLoop/footer.php
@@ -0,0 +1,9 @@
+	</section>
+	<footer class="flex-container  b-light-gray">
+		<div class="footer  flex-auto  sm-w100  b-dark-gray  border">
+			<p>Pié de Página</p>
+		</div>
+	</footer>
+	<?php wp_footer(); ?>
+</body>
+</html>
